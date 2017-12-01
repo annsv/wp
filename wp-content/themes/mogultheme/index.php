922c313998c1a5f5b7b04721b9e3d5134c13128a
Anna Sebova <annablks@gmail.com>
2017-12-02 00:19:24 +0200

    adds bootstrap to mogultheme style file

--- wp-content/themes/mogultheme/index.php
+++ wp-content/themes/mogultheme/index.php
@@ -22,10 +22,6 @@ get_header(); ?>
 		<header class="page-header">
 			<h1 class="page-title"><?php single_post_title(); ?></h1>
 		</header>
-	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
-	</header>
 	<?php endif; ?>
 
 	<div id="primary" class="content-area">
@@ -46,15 +42,9 @@ get_header(); ?>
 
 				endwhile;
 
-				the_posts_pagination( array(
-					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-				) );
-
 			else :
 
-				get_template_part( 'template-parts/post/content', 'none' );
+				get_template_part( 'template-parts/content', 'none' );
 
 			endif;
 			?>
